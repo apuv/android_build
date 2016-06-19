91e9bcfddf69c09b6a6142d79e37625e6ab3b06c
Apu V <theapu@gmail.com>
2016-06-19 08:44:11 -0400

    compiler changed to ubertc

--- envsetup.sh
+++ envsetup.sh
@@ -155,6 +155,8 @@ function setpaths()
     targetgccversion=$(get_build_var TARGET_GCC_VERSION)
     targetgccversion2=$(get_build_var 2ND_TARGET_GCC_VERSION)
     targetlegacygccversion=$(get_build_var TARGET_LEGACY_GCC_VERSION)
+    targetgccversion="uber-4.9"
+    targetlegacygccversion="uber-4.8"
     export TARGET_GCC_VERSION=$targetgccversion
 
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
