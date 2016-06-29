8e489457e09e6ff2d579f231e288f2e32ebe05e0
Apu V <theapu@gmail.com>
2016-06-29 21:45:19 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -155,8 +155,8 @@ function setpaths()
     targetgccversion=$(get_build_var TARGET_GCC_VERSION)
     targetgccversion2=$(get_build_var 2ND_TARGET_GCC_VERSION)
     targetlegacygccversion=$(get_build_var TARGET_LEGACY_GCC_VERSION)
-    targetgccversion="uber-5.3"
-    targetlegacygccversion="uber-5.3"
+    targetgccversion="uber-4.9"
+    targetlegacygccversion="uber-4.9"
     export TARGET_GCC_VERSION=$targetgccversion
 
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
