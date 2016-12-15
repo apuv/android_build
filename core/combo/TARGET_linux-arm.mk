08fb8e82b7b2f27d1f562a1687043f788edbb526
Apu V <theapu@gmail.com>
2016-12-15 21:19:48 +0530

    Update TARGET_linux-arm.mk

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -34,13 +34,16 @@ TARGET_$(combo_2nd_arch_prefix)ARCH_VARIANT := armv5te
 endif
 
 # Decouple NDK library selection with platform compiler version
-$(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := 4.9
+#$(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := 4.9
 #$(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := uber-5.3
+$(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := uber-4.9
 
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
+$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := uber-4.9
+$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := uber-4.9
 #$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := uber-5.3
 #$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := uber-5.3
-$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := 4.9
+#$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := 4.9
 else
 $(combo_2nd_arch_prefix)TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
@@ -118,7 +121,8 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += \
 # into no-op in some builds while mesg is defined earlier. So we explicitly
 # disable "-Wunused-but-set-variable" here.
 ##ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.9 uber-4.8 uber-4.9 uber 5.3 uber-4.8.% uber-4.9.% uber-5.3.%, $($(combo_2nd_arch_prefix)TARGET_GCC_VERSION)),)
-ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.9, $($(combo_2nd_arch_prefix)TARGET_GCC_VERSION)),)
+##ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.9, $($(combo_2nd_arch_prefix)TARGET_GCC_VERSION)),)
+ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.9 uber-4.8 uber-4.9 uber 5.3 uber-4.8.% uber-4.9.% uber-5.3.%, $($(combo_2nd_arch_prefix)TARGET_GCC_VERSION)),)
 $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += -fno-builtin-sin \
 			-fno-strict-volatile-bitfields
 endif
